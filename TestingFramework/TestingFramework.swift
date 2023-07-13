c434c0e76a3f0ddc3d2e9b3f43f48f0b979bfb2f
Vijay Vikram Singh <svijayvikram00@gmail.com>
2023-07-13 17:17:27 +0530

    not tracked file added

---
+++ TestingFramework/TestingFramework.swift
@@ -0,0 +1,23 @@
+//
+//  TestingFramework.swift
+//  TestingFramework
+//
+//  Created by Vijay Singh on 13/07/23.
+//
+
+import Foundation
+
+
+public final class TestingFramework {
+
+    let name = "TestingFramework"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
