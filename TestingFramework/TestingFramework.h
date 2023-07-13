a089eaec5d104184b31beee8c7a5e0b485e6d355
Vijay Vikram Singh <svijayvikram00@gmail.com>
2023-07-13 16:51:15 +0530

    initial commit

---
+++ TestingFramework/TestingFramework.h
@@ -0,0 +1,18 @@
+//
+//  TestingFramework.h
+//  TestingFramework
+//
+//  Created by Vijay Singh on 13/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestingFramework.
+FOUNDATION_EXPORT double TestingFrameworkVersionNumber;
+
+//! Project version string for TestingFramework.
+FOUNDATION_EXPORT const unsigned char TestingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestingFramework/PublicHeader.h>
+
+
