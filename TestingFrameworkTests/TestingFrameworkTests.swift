2579a96f26386e3090f08287b82b4c7c1d5caeb0
Vijay Vikram Singh <svijayvikram00@gmail.com>
2023-07-13 17:15:27 +0530

    testcase and library implemented

--- TestingFrameworkTests/TestingFrameworkTests.swift
+++ TestingFrameworkTests/TestingFrameworkTests.swift
@@ -10,27 +10,14 @@ import XCTest
 
 final class TestingFrameworkTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var testingFrame = TestingFramework()
+    
+    func testAddFunction_equals(){
+        XCTAssertEqual(testingFrame.add(a: 1, b: 1), 2)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSubFunction_equals(){
+        XCTAssertEqual(testingFrame.sub(a: 5, b: 4), 1)
     }
 
 }
